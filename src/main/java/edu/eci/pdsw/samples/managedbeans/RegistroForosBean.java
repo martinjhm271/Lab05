56c3173e28ff1d8b22bd62cf53bddbcbfeac6315
Martin Hernandez <martinjhm271@hotmail.es>
2016-09-29 10:01:06 -0500

    forosbean

--- src/main/java/edu/eci/pdsw/samples/managedbeans/RegistroForosBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/RegistroForosBean.java
@@ -17,20 +17,114 @@
 package edu.eci.pdsw.samples.managedbeans;
 
 
+import edu.eci.pdsw.samples.entities.Comentario;
+import edu.eci.pdsw.samples.entities.EntradaForo;
+import edu.eci.pdsw.samples.entities.Usuario;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosForos;
 import edu.eci.pdsw.samples.services.ServiciosForo;
+import edu.eci.pdsw.samples.services.ServiciosForoStub;
 import java.io.Serializable;
-import javax.annotation.ManagedBean;
-import javax.enterprise.context.SessionScoped;
+import java.sql.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 
 /**
  *
  * @author hcadavid
  */
-@ManagedBean
+@ManagedBean(name="RegistroForo")
 @SessionScoped
 public class RegistroForosBean implements Serializable{
     
     ServiciosForo sp=ServiciosForo.getInstance();
+    EntradaForo selectForo;
+    
+    private int identificador;
+    
+    private Usuario autor;
+    
+    private String comentario;
+    
+    private Set<Comentario> respuestas;
+    
+    private String titulo;
+    
+    private Date fechayHora;
+    
+    public List<EntradaForo> getEntradasForo() throws ExcepcionServiciosForos{
+        return sp.consultarEntradasForo();
+    }
+
+    public EntradaForo getSelectForo() {
+        return selectForo;
+    }
+
+    public void setSelectForo(EntradaForo selectForo) {
+        this.selectForo = selectForo;
+    }
+    
+    public void setEntradasForo(){
+        EntradaForo ef=new EntradaForo(getIdentificador(),getAutor(),  getComentario(),  getTitulo(),  getFechayHora());
+        try{
+            sp.registrarNuevaEntradaForo(ef);
+        } catch (ExcepcionServiciosForos ex) {
+            Logger.getLogger(ServiciosForoStub.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Set<Comentario> getRespuestas() {
+        return respuestas;
+    }
+
+    public void setRespuestas(Set<Comentario> respuestas) {
+        this.respuestas = respuestas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFechayHora() {
+        return fechayHora;
+    }
+
+    public void setFechayHora(Date fechayHora) {
+        this.fechayHora = fechayHora;
+    }
     
     
 }
