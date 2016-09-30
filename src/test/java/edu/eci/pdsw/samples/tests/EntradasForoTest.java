5b9bb39962a4bea6f4e747ab302a58d6ad7de943
Martin Hernandez <martinjhm271@hotmail.es>
2016-09-29 21:00:17 -0500

    actualizacion casos de prueba

--- src/test/java/edu/eci/pdsw/samples/tests/EntradasForoTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/EntradasForoTest.java
@@ -38,11 +38,11 @@ import java.util.List;
 /**
  * Pruebas de Equivalencia
  * 
- * Clase 1 = Enviarle un foro con un identificador no valido , deberia no asignarlo
+ * Clase 1 = Enviarle un foro con un identificador no valido id<0 o id >999999.
  * 
- * Clase 2 = Enviarle un foro hecho por un usuario temporal que no este registrado en la  base de datos , se espera que falle
+ * Clase 2 = Enviarle un foro con un titulo y comentario nulo o vacio (titulo="" || comentario="" || titulo=null || comentario=null)
  * 
- * Clase 3 = Enviarle un foro con sin un usuario, se espera que falle
+ * Clase 3 = Enviarle un foro con un uuario que tenga de email y nombre nulo o vacio (titulo="" || comentario="" || titulo=null || comentario=null) 
  *
  */
 public class EntradasForoTest {
@@ -68,6 +68,21 @@ public class EntradasForoTest {
     	for(int i=0;i<temp.size() && ban;i++){if(temp.get(i).equals(ef)){id=temp.get(i).getIdentificador();ban=false;}}
     	assertNotEquals(999999,id);
     }
-    
+     @Test
+    public void agregarforo2() throws ExcepcionServiciosForos{
+    	ServiciosForoStub svf=new ServiciosForoStub();
+    	EntradaForo ef=new EntradaForo(999999,new Usuario("martinjhm271@hotmail.es","Martin Hernandez") ,"", "", java.sql.Date.valueOf("2000-01-01"));
+    	svf.registrarNuevaEntradaForo(ef);
+    	List<EntradaForo> temp =svf.consultarEntradasForo();
+    	assertEquals("Deberia agregar el nuevo foro con un titulo y comentario vacio",temp.size(),2);
+    }
+    @Test
+    public void agregarforo3() throws ExcepcionServiciosForos{
+    	ServiciosForoStub svf=new ServiciosForoStub();
+    	EntradaForo ef=new EntradaForo(999999,new Usuario("","") ,"Por que el agua moja?", "Super interrogante", java.sql.Date.valueOf("2000-01-01"));
+    	svf.registrarNuevaEntradaForo(ef);
+    	List<EntradaForo> temp =svf.consultarEntradasForo();
+    	assertEquals("Deberia agregar el nuevo foro con un usuario que tiene nombre y email vacio",temp.size(),2);
+    }
     
 }
